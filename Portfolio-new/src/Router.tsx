234bff7b47081d0439dbc3d454fe955add245904
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-08-23 11:54:01 +0200

    First commit, project setup

---
+++ Portfolio-new/src/Router.tsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+//import Layout from "./components/Layout/Layout"; 
+import HomePage from "./pages/Home/HomePage";
+import ContactPage from "./pages/Contact/ContactPage";
+import ProjectsPage from "./pages/Projects/ProjectsPage"
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
+import AboutPage from "./pages/About/AboutPage";
+
+
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+   // element: <Layout></Layout>,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: (
+            <>
+              <HomePage />
+            </>
+          ),
+        index: true,
+      },
+      {
+        path: "/projects",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
---
+++ Portfolio-new/src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col">
+        Homepage
+      <div className="flex justify-center items-center">
+      </div>
+    </div>
+  );
+};
+
+export default HomePage
