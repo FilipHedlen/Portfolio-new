4903046f15834361e1818d9696d28999f720b27b
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-09-09 12:41:17 +0200

    Added a touch swipe effect on project cards to simplify mobile use

--- Portfolio-new/src/components/ProjectCards/ProjectCarousel/ProjectCarousel.tsx
+++ Portfolio-new/src/components/ProjectCards/ProjectCarousel/ProjectCarousel.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ProjectCards from '../ProjectCards/ProjectCards';
 import { projects } from '../../../data/projectData';
 
 const ProjectsCarousel: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const touchStartX = useRef<number>(0);
+  const touchEndX = useRef<number>(0);
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
@@ -13,10 +15,31 @@ const ProjectsCarousel: React.FC = () => {
     setCurrentIndex((prevIndex) => (prevIndex === projects.length - 1 ? 0 : prevIndex + 1));
   };
 
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    touchEndX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStartX.current - touchEndX.current > 50) {
+      handleNext(); // Swipe left
+    } else if (touchEndX.current - touchStartX.current > 50) {
+      handlePrev(); // Swipe right
+    }
+  };
+
   return (
-    <div className="relative min-h-screen w-full max-w-[800px] mx-auto px-4 sm:px-6 lg:px-8 touch-pan-x">
+    <div
+      className="relative min-h-screen w-full max-w-[800px] mx-auto px-4 sm:px-6 lg:px-8"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className="overflow-hidden">
-        <div 
+        <div
           className={`flex transition-transform duration-500 ease-in-out`}
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
@@ -39,7 +62,7 @@ const ProjectsCarousel: React.FC = () => {
           ))}
         </div>
       </div>
-      <div className="absolute top-52 left-0 right-0 flex justify-between px-5">
+      <div className="absolute top-52 left-0 right-0 justify-between px-5 hidden sm:flex">
         <button
           onClick={handlePrev}
           className="text-4xl sm:text-5xl text-dark-grey dark:text-light-grey hover:text-primary-light-hover dark:hover:text-primary-dark-hover transition duration-100 p-2 sm:p-4"
@@ -48,7 +71,7 @@ const ProjectsCarousel: React.FC = () => {
         </button>
         <button
           onClick={handleNext}
-          className="text-4xl sm:text-5xl color text-dark-grey dark:text-light-grey hover:text-primary-light-hover dark:hover:text-primary-dark-hover transition duration-100 p-2 sm:p-4"
+          className="text-4xl sm:text-5xl text-dark-grey dark:text-light-grey hover:text-primary-light-hover dark:hover:text-primary-dark-hover transition duration-100 p-2 sm:p-4"
         >
           ❯
         </button>
