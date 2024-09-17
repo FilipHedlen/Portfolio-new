4537cfe4185b14bfbfd3d0ebb4a4fea2c252ed7d
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-09-17 18:24:00 +0200

    Config done for emailjs, separated contactform to a separate component

--- Portfolio-new/src/components/Contact/ContactForm.tsx
+++ Portfolio-new/src/components/Contact/ContactForm.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
 
 const ContactForm = () => {
-  return (
-    <div>ContactForm</div>
-  )
-}
+  const [isMailSent, setIsMailSent] = useState(false);
 
-export default ContactForm
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+    const form = e.currentTarget;
+
+    if (form) {
+      try {
+        await emailjs.sendForm(serviceId, templateId, e.currentTarget, publicKey);
+          setIsMailSent(true);
+        } catch (error) {
+          console.error("Error sending email", error);
+        }
+        form.reset();
+    }
+  };
+
+  return (
+    <div className="w-full sm:w-[90%] max-w-lg mx-8 my-16">
+      {isMailSent ? (
+        <div className="bg-transparent mx-auto p-4 mb-4 width flex justify-center w-[50%]">
+          <h3 className="text-2xl font-lora text-dark-grey dark:text-light-grey">
+            Your mail has been sent successfully!
+          </h3>
+        </div>
+      ) : (
+        <div className="flex-1 mt-12 sm:max-w-lg lg:max-w-md">
+          <form onSubmit={handleSubmit} className="space-y-5">
+            <div>
+              <label className="font-medium">Full name</label>
+              <input
+                type="text"
+                name="fullName"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Email</label>
+              <input
+                type="email"
+                name="email"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Message</label>
+              <textarea
+                name="message"
+                required
+                className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ContactForm;
--- Portfolio-new/src/components/Contact/ContactSection.tsx
+++ Portfolio-new/src/components/Contact/ContactSection.tsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaLinkedin, FaPhoneAlt } from "react-icons/fa"
 import { IoIosMail } from "react-icons/io"
+import ContactForm from "./ContactForm"
 
 export const ContactSection = () => {
   const contactMethods = [
@@ -51,72 +52,35 @@ export const ContactSection = () => {
 		},
   ]
 
-    return (
-      <main className="py-14 animate-spin">
-          <div className="max-w-screen-xl mx-auto px-4 text-dark-grey dark:text-light-grey md:px-8">
-              <div className="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none">
-                  <div className="max-w-lg space-y-3">
-                      <p className="text-dark-grey dark:text-light-grey text-3xl font-semibold sm:text-4xl">
-                        Contact Information
-                      </p>
-                      <p className="mb-20">
-                        Feel free to contact me if you have any questions about anything, or if you just want to connect!
-                      </p>
+  return (
+    <main className="py-14 animate-spin">
+      <div className="max-w-screen-xl mx-auto px-4 text-dark-grey dark:text-light-grey md:px-8">
+        <div className="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none">
+          <div className="max-w-lg space-y-3">
+            <p className="text-dark-grey dark:text-light-grey text-3xl font-semibold sm:text-4xl">
+              Contact Information
+            </p>
+            <p className="mb-20">
+              Feel free to contact me if you have any questions about anything, or if you just want to connect!
+            </p>
+            <div>
+              <ul className="mt-6 flex flex-col flex-wrap gap-y-6">
+                {
+                  contactMethods.map((item, idx) => (
+                    <li key={idx} className="flex items-center gap-x-3">
                       <div>
-                        <ul className="mt-6 flex flex-col flex-wrap gap-y-6">
-                          {
-                            contactMethods.map((item, idx) => (
-                              <li key={idx} className="flex items-center gap-x-3">
-                                <div>
-                                  {item.icon}
-                                </div>
-                                <p>{item.contact}</p>
-                              </li>
-                              ))
-                          }
-                        </ul>
+                        {item.icon}
                       </div>
-                    </div>
-                    <div className="flex-1 mt-12 sm:max-w-lg lg:max-w-md">
-                      <form
-                        onSubmit={(e) => e.preventDefault()}
-                        className="space-y-5"
-                      >
-                        <div>
-                          <label className="font-medium">
-                            Full name
-                          </label>
-                          <input
-                            type="text"
-                            required
-                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                          />
-                        </div>
-                        <div>
-                          <label className="font-medium">
-                            Email
-                          </label>
-                          <input
-                            type="email"
-                            required
-                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                          />
-                        </div>
-                        <div>
-                          <label className="font-medium">
-                            Message
-                          </label>
-                          <textarea required className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"></textarea>
-                        </div>
-                        <button
-                          className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
-                        >
-                          Submit
-                        </button>
-              </form>
+                      <p>{item.contact}</p>
+                    </li>
+                  ))
+                }
+              </ul>
             </div>
+            <ContactForm />
           </div>
         </div>
-      </main>
+        </div>
+    </main>
   )
 }
